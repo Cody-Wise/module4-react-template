4ccdba9a837f6688d79811de636797d694f293b9
Cody-Wise <cody@sharptackmedia.com>
2022-07-26 16:39:44 -0700

    Updated global colors and added in fonts

--- src/components/Page/Footer/Footer.jsx
+++ src/components/Page/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import styles from './Footer.css';
 //made footer component
 export default function Footer() {
   return (
-    <footer className={styles.header}>
+    <footer className={styles.footer}>
       <div>Footer</div>
     </footer>
   );
